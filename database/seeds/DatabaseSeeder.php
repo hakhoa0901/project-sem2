d89ad024009fb83b7b6c7961dbe6e722e0aafaa2
Dat <datnvth1907029@fpt.edu.vn>
2020-07-31 19:50:32 +0700

    Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+
+       $this->call(CategoriesSeeder::class);
+       $this->call(BrandsSeeder::class);
+       $this->call(AccountsSeeder::class);
+        $this->call(ProductsSeeder::class);
     }
 }
